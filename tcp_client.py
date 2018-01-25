102e9f67c9b782002b6711f0d192ecda454e6606
LeeYee <371629179@qq.com>
2018-01-25 06:46:29 +0000

    first commit tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
+# -*- coding=utf-8 -*-
 
 import socket
+import os
+import sys
+import struct
+import time
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+def socket_client_recv():
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(('192.168.0.103',6666))
+        except socket.error as msg:
+            print(msg)
+            sys.exit(1)
+        print("connecting service...")
+        
+        data_from_service = s.recv(64)
+        print(data_from_service)
+        
+        data_from_service = s.recv(64)
+        print(data_from_service)
+        
+        time.sleep(1)
 
-s.connect(('192.168.0.102',8080))
+        while 1:
+            data_from_service = s.recv(64)
+            if(data_from_service):
+                print(data_from_service)
 
-s.send(b'123')
 
-while(1):
-    data = s.recv(8)
-    #if data == b'l':
-    print(data)
+if __name__ == '__main__':
+        socket_client_recv()
     
 
